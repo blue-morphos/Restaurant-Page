03ca65b3ba3752bf5c4f425d06d22df230ce9855
Andrew Prinn <andrewprinn@gmail.com>
2022-12-13 17:29:45 -0500

    added hours to contact page

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,8 +1,12 @@
 export const contactFunction = () => {
-  const content = document.getElementById("content");
-  const address = document.createElement("h1");
-  address.textContent = "37 Spooner St, Quahog, RI, USA";
-  const phone = document.createElement("h1");
-  phone.textContent = "+1 234-567-8900";
-  content.append(address, phone);
+  const content = document.getElementById("content")
+  const address = document.createElement("h1")
+  address.textContent = "37 Spooner St, Quahog, RI, USA"
+  const phone = document.createElement("h1")
+  phone.textContent = "+1 234-567-8900"
+  const hours = document.createElement("h1")
+  hours.textContent = "Open 5:30pm – 10:00pm Tuesday – Saturday"
+  const requireReservation = document.createElement("h1");
+  requireReservation.textContent = "Reservation Required"
+  content.append(address, phone, hours, requireReservation)
 };
