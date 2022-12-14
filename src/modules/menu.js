90af623178706e3aed29cc46a1f3d1b62eb65322
Andrew Prinn <andrewprinn@gmail.com>
2022-12-13 21:28:15 -0500

    updated menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,18 +1,59 @@
 export const menuFunction = () => {
+  const content = document.getElementById("content")
   const menu = document.createElement("div")
-  menu.id="menu"
+  menu.setAttribute('class', 'menu')
+  menu.setAttribute("id", "menu")
   content.appendChild(menu)
   const food1 = document.createElement("div")
-  food1.textContent="Wagyu burger, $13"
+  const food1text = document.createTextNode("Striped Sea Bass, $36")
+  food1.appendChild(food1text)
+  
   const food2 = document.createElement("div")
-  food2.textContent = "Tomahawk steak, $38"
+  const food2text = document.createTextNode("Lamb Shank, $32")
+  food2.appendChild(food2text)
+
   const food3 = document.createElement("div")
-  food3.textContent = "Split pea soup, $9"
+  const food3text = document.createTextNode("Swordfish Burger, $31")
+  food3.appendChild(food3text)
+
   const food4 = document.createElement("div")
-  food4.textContent = "Chef salad, $14"
+  const food4text = document.createTextNode("Braised Beef Cheek, $28")
+  food4.appendChild(food4text)
+
   const food5 = document.createElement("div")
-  food5.textContent = "Devil's food cake, $9"
+  const food5text = document.createTextNode("Ratatouille, $26")
+  food5.appendChild(food5text)
+
   const food6 = document.createElement("div")
-  food6.textContent = "Apple pie, $9"
-  menu.append(food1, food2, food3, food4, food5, food6)
+  const food6text = document.createTextNode("Salmon fillet, $30")
+  food6.appendChild(food6text)
+
+  const food7 = document.createElement("div")
+  const food7text = document.createTextNode("New York Steak, $36")
+  food7.appendChild(food7text)
+
+  const food8 = document.createElement("div")
+  const food8text = document.createTextNode("Tri-Tip, $36")
+  food8.appendChild(food8text)
+
+  const food9 = document.createElement("div")
+  const food9text = document.createTextNode("Duck Confit, $26")
+  food9.appendChild(food9text)
+
+  const food10 = document.createElement("div")
+  const food10text = document.createTextNode("Chocolate Soufflé, $22")
+  food10.appendChild(food10text
+)
+  
+  const food11 = document.createElement("div")
+  const food11text = document.createTextNode("Profiterole, $12")
+  food11.appendChild(food11text
+)
+
+  const food12 = document.createElement("div")
+  const food12text = document.createTextNode("Crème brûlée, $12")
+  food12.appendChild(food12text
+)
+
+ menu.append(food1, food2, food3, food4, food5, food6, food7, food8, food9, food10, food11, food12)
 };
--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -1,10 +1,21 @@
 export const initialPageLoadFunction = () => {
   const content = document.getElementById("content");
-  const h1 = document.createElement("h1");
-  h1.textContent = "Le Meilleur dans le Monde";
-  const copy = document.createElement("p");
-  copy.textContent =
-    "Named the country's best restaraunt by Redundant Magazine test";
   const image = document.createElement("IMG");
-  content.append(h1, copy, image);
+  image.setAttribute('src', 'https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"')
+  const para = document.createElement("div");
+  para.setAttribute('class', 'para')
+  const p1 = document.createElement("p")
+  p1.textContent = 
+    "Named the country's best restaraunt by Redundant Magazine"
+  const p2 = document.createElement("p")
+  p2.textContent = 
+    "Located in the heart of City Circle, Le Meilleur dans le Monde is an elegant dining destination with downtown city views. Featuring modern American cuisine with French influences, The National Paper described Le Meilluer dans le Monde as “the finest in the world.”"
+  const p3 = document.createElement("p")
+  p3.textContent = 
+    "The incredible architecture, designed by Billy Bob Thornton, features a solid gold toilet, 100% diamond windows, edible white chocolate walls, artwork by Van Gogh, and panoramic views of the city, while a seasonal terrace offers al fresco dining from spring to fall."
+  const p4 = document.createElement("p")
+  p4.textContent = 
+    "Reserve your table today to see why Le Meilluer dans le Monde is considered the best in the world."
+  para.append(p1, p2, p3, p4)
+  content.append(image, para);
 };
